5b407273f54d0983c5818c449dcf26e6aa4f7eff
ahmeddkhann <iamahmedkhan02@gmail.com>
2025-01-30 12:40:01 +0500

    bassic structure is ready

--- management.py
+++ management.py
@@ -1,1 +1,45 @@
-print("welcome to youtube management app")
+def load_data():
+    pass
+
+def list_all_videos(videos):
+    pass
+
+def add_video(videos):
+    pass
+
+def update_video(videos):
+    pass
+
+def delete_video(videos):
+    pass
+
+
+def main():
+    videos = load_data()
+    while True:
+        print(" ***** Youtube managemnt app *****")
+        print("Choices you want to opearate.")
+        print("1. list all the favourite videos.")
+        print("2. Add new video.")
+        print("3. Update video details.")
+        print("4. Delete Video.")
+        print("5. Exit.")
+
+        choice = input("enter the choice: ")
+        
+        match choice:
+            case "1":
+                list_all_videos(videos)
+            case "2":
+                add_video(videos)
+            case "3":
+                update_video(videos)
+            case "4":
+                delete_video(videos)
+            case "5":
+                break
+            case _:
+                print("Invalid choice. Please choose a valid option.")
+
+if __name__ == "__main__":
+    main()
